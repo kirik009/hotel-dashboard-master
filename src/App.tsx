87ab2b31b2836b3ba10ada07415f1fb83a93c909
kirillboravik69 <kirillboravik69@gmail.com>
2024-09-11 21:39:17 +0300

    lastCommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React, { Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import { Route, Routes } from "react-router-dom";
 const Card = React.lazy(() => import("./components/Card"));
 const Create = React.lazy(() => import("./components/Create"));
 const Destinations = React.lazy(() => import("./components/Destinations"));
 const Details = React.lazy(() => import("./components/Details"));
 const NotFound = React.lazy(() => import("./components/NotFound"));
-const ReactQueryDestinations = React.lazy(
-  () => import("./components/ReactQueryDestinations")
-);
-const Search = React.lazy(() => import("./components/Search"));
 
 function App() {
   const queryClient = new QueryClient();
@@ -24,10 +19,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
           <Route path="/" element={<Card />} />
           <Route path="/destinations" element={<Destinations />} />
-          <Route path="/react-query" element={<ReactQueryDestinations />} />
-          <Route path="/search" element={<Search />} />
         </Routes>
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </Suspense>
   );
